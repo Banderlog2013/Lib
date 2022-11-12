f495e05cc6b4f14547f88ce4f2d4c0230ae2d191
banderlog2013 <banderlog2013@yandex.ru>
2022-11-12 14:26:04 +0300

    handlers

--- src/js/lib/core.js
+++ src/js/lib/core.js
@@ -6,6 +6,13 @@ $.prototype.init = function(selector) {
     if (!selector) {
         return this; // {} возращает пустой объект
     }
+
+    if(selector.tagName) {
+        this[0] = selector;
+        this,this.length = 1;
+        return this;
+    }
+
     Object.assign(this, document.querySelectorAll(selector));
     this.length = document.querySelectorAll(selector).length;
     return this;
