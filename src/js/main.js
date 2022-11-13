c803181f93cc9fbac4f35717bcbff18762931eb8
banderlog2013 <banderlog2013@yandex.ru>
2022-11-13 21:22:55 +0300

    effects

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,15 @@
 import $ from './lib/lib';
 
 $('button').on('click', function() {
-    $(this).toggleClass('active');
+    $('div').eq(1).toggleClass('active');
 });
 
+$('div').click(function() {
+    console.log($(this).index());
+});
 
+// console.log($('div').eq(2).find('.some'));
+// console.log($('.some').closest('.findme'))
+// console.log($('.more').eq(0).siblings());
+$('button').eq(0).fadeIn(1800);
+// console.log($('button').html('hello'));
